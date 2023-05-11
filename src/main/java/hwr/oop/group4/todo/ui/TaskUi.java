7a5a45fc33e462aefa8de720c80e2eb1d84cee83
Philip Berner <philipberner.berlin@gmail.com>
2023-05-11 13:35:09 +0200

    added open, progress, complete, remove and remove all done

--- src/main/java/hwr/oop/group4/todo/ui/TaskUi.java
+++ src/main/java/hwr/oop/group4/todo/ui/TaskUi.java
@@ -1,9 +1,7 @@
 package hwr.oop.group4.todo.ui;
 
-import hwr.oop.group4.todo.core.Idea;
-import hwr.oop.group4.todo.core.Project;
-import hwr.oop.group4.todo.core.Task;
-import hwr.oop.group4.todo.core.TodoList;
+import hwr.oop.group4.todo.commons.exceptions.TodoRuntimeException;
+import hwr.oop.group4.todo.core.*;
 import hwr.oop.group4.todo.ui.controller.ConsoleController;
 import hwr.oop.group4.todo.ui.controller.ConsoleHelper;
 import hwr.oop.group4.todo.ui.controller.command.Command;
@@ -15,17 +13,15 @@ import hwr.oop.group4.todo.ui.controller.tables.ColumnConfig;
 import hwr.oop.group4.todo.ui.controller.tables.Table;
 
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class TaskUi {
     private final ConsoleController consoleController;
     private final ConsoleHelper consoleHelper;
 
-    private List<Task> tasks;
+    private List<String> prefixes;
+    private Set<Task> tasks;
 
     public TaskUi(ConsoleController consoleController) {
         this.consoleController = consoleController;
@@ -33,19 +29,39 @@ public class TaskUi {
     }
 
     public void menu(TodoList todoList) {
-        menu(todoList.getLoseTasks().stream().toList(), List.of("tasks"));
+        menu(todoList.getLoseTasks(), List.of("tasks"));
     }
 
     public void menu(Project project, List<String> prefixes) {
         final List<String> modifiable = new ArrayList<>(prefixes);
         modifiable.add("task");
-        menu(project.getTasks().stream().toList(), Collections.unmodifiableList(modifiable));
+        menu(project.getTasks(), Collections.unmodifiableList(modifiable));
     }
 
-    private void menu(List<Task> tasks, List<String> prefixes) {
+    private void menu(Set<Task> tasks, List<String> prefixes) {
         this.tasks = tasks;
+        this.prefixes = prefixes;
 
-        final String menu =  new Menu("Task Menu", "List and Edit your Tasks." , List.of(
+        consoleController.output(getMenu());
+        list(null);
+        final AtomicBoolean shouldReturn = new AtomicBoolean(false);
+        while (!shouldReturn.get()) {
+            consoleController.inputOptions(prefixes, List.of(
+                    new Command("list",   this::list),
+                    new Command("new",    this::create),
+                    new Command("edit",   this::editTask),
+                    new Command("remove", this::remove),
+                    new Command("removeAllDone", this::removeAllDone),
+                    new Command("complete", this::complete),
+                    new Command("inProgress", this::progress),
+                    new Command("open", this::open),
+                    new Command("back",   args -> shouldReturn.set(true))
+            ), new Command("wrongInput", args -> {}));
+        }
+    }
+
+    private String getMenu() {
+        return new Menu("Task Menu", "List and Edit your Tasks." , List.of(
                 new Entry("list", "List all tasks."),
                 new Entry("new", "Create a new task."),
                 new Entry("edit", "Edit the attributes of a project.", List.of(
@@ -67,29 +83,16 @@ public class TaskUi {
                 new Entry("progress", "Mark a task as in progress.", List.of(
                         new EntryArgument("id <id>", "ID of the task which is to be set to in progrss.")
                 )),
+                new Entry("open", "Reopens a task.", List.of(
+                        new EntryArgument("id <id>", "ID of the task which is to be set to open.")
+                )),
                 new Entry("back", "Returns to the previous menu."),
                 new Entry("help", "Prints this Menu again.")
         )).toString();
-
-        consoleController.output(menu);
-        listTasks(null);
-        final AtomicBoolean shouldReturn = new AtomicBoolean(false);
-        while (!shouldReturn.get()) {
-            consoleController.inputOptions(List.of("loseTasks"), List.of(
-                    new Command("list",   this::listTasks),
-                    new Command("new",    this::createTask),
-                    new Command("edit",   this::editTask),
-                    new Command("remove", this::removeTask),
-                    new Command("removeAllDone", this::removeAllDone),
-                    new Command("removeAllDone", this::completeTask),
-                    new Command("removeAllDone", this::progressTask),
-                    new Command("back",   args -> shouldReturn.set(true))
-            ), new Command("wrongInput", args -> {}));
-        }
     }
 
 
-    private void listTasks(Collection<CommandArgument<String>> args) {
+    private void list(Collection<CommandArgument<String>> args) {
         final int idColumnLength = Math.max((int) Math.ceil(Math.log10(tasks.size()) - 2), 2);
         final Table projectTable = new Table(List.of(
                 new ColumnConfig("ID", idColumnLength),
@@ -101,8 +104,9 @@ public class TaskUi {
                 new ColumnConfig("Status", 10)
         ));
 
-        for (int i = 0; i < tasks.size(); i++) {
-            final Task task = tasks.get(i);
+        final List<Task> taskList = tasks.stream().toList();
+        for (int i = 0; i < taskList.size(); i++) {
+            final Task task = taskList.get(i);
             projectTable.addRow(
                     String.valueOf(i),
                     task.getName(),
@@ -117,28 +121,56 @@ public class TaskUi {
         consoleController.output(projectTable.toString());
     }
 
-    private void createTask(Collection<CommandArgument<String>> args) {
+    private void create(Collection<CommandArgument<String>> args) {
 
     }
 
-    private void createTask(Idea idea) {
+    private void create(Idea idea) {
+    }
+
+    private Optional<Task> getTaskFromId(Collection<CommandArgument<String>> args) {
+        final int id;
+        try {
+            id = consoleHelper.getId(args, tasks.size());
+        } catch (TodoRuntimeException e) {
+            consoleController.outputLine(e.getMessage());
+            return Optional.empty();
+        }
+        return Optional.ofNullable(tasks.stream().toList().get(id));
     }
 
     private void editTask(Collection<CommandArgument<String>> args) {
+
     }
 
-    private void progressTask(Collection<CommandArgument<String>> args) {
+    private void open(Collection<CommandArgument<String>> args) {
+        getTaskFromId(args).ifPresent(Task::open);
     }
 
-    private void completeTask(Collection<CommandArgument<String>> args) {
+    private void progress(Collection<CommandArgument<String>> args) {
+        getTaskFromId(args).ifPresent(Task::inProgress);
+    }
+
+    private void complete(Collection<CommandArgument<String>> args) {
+        getTaskFromId(args).ifPresent(Task::closed);
     }
 
     private void removeAllDone(Collection<CommandArgument<String>> args) {
+        tasks.removeIf(task -> task.getStatus() == Status.CLOSED);
     }
 
-    private void removeTask(Collection<CommandArgument<String>> args) {
+    private void remove(Collection<CommandArgument<String>> args) {
+        final List<String> removePrefix = new ArrayList<>(prefixes);
+        removePrefix.add("remove");
+        removePrefix.add(consoleHelper.getStringParameter(args, "id").orElse("?"));
+
+        final Optional<Task> task = getTaskFromId(args);
+        if (task.isEmpty()) {
+            return;
+        }
+        final String confirmation = "Do you really want to remove " + task.get().getName() + "?";
+        if (consoleController.inputBool(removePrefix, confirmation, false)) {
+            tasks.remove(task.get());
+        }
     }
-
-
-
 }
