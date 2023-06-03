f6bda1ff12c897e20651dda60fb2860e5000c0f9
Marvin Waldmann <marvin.waldmann@sap.com>
2023-06-03 09:45:17 +0200

    comments with old code removed

--- src/main/java/hwr/oop/group4/todo/persistence/PersistableTodoList.java
+++ src/main/java/hwr/oop/group4/todo/persistence/PersistableTodoList.java
@@ -27,37 +27,6 @@ public class PersistableTodoList implements Persistable {
 
     @Override
     public void importFromString(String dataString) {
-        /*JSONObject todoListObject = new JSONObject(dataString);
 
-        JSONArray projectsArray = todoListObject.getJSONArray("projects");
-        JSONArray inTrayArray = todoListObject.getJSONArray("inTray");
-        for (int i = 0; i < inTrayArray.length(); i++) {
-            Idea idea = importIdea(inTrayArray.getJSONObject(i));
-            todoList.addIdea(idea);
-        }
-        JSONArray loseTasksArray = todoListObject.getJSONArray("loseTasks");
-        for (int i = 0; i < loseTasksArray.length(); i++) {
-            Task task = importTask(loseTasksArray.getJSONObject(i));
-            todoList.addLoseTask(task);
-        }
-        JSONArray somedayMaybeArray = todoListObject.getJSONArray("somedayMaybe");*/
     }
-
-    /*private Task importTask(JSONObject taskObject) {
-        return new Task.TaskBuilder()
-                .name(taskObject.getString("name"))
-                .description(taskObject.getString("description"))
-                // TODO: deadline
-                .priority(taskObject.getInt("priority"))
-                // TODO: tags
-                .status(taskObject.getEnum(Status.class, "status"))
-                .build();
-    }
-
-    private Idea importIdea(JSONObject ideaObject) {
-        return new Idea(
-                ideaObject.getString("name"),
-                ideaObject.getString("description")
-        );
-    }*/
 }
